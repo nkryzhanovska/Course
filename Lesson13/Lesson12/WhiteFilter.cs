90f1e976b7606744ea68ee5a1cda02944ce7939d
Natalia Kryzhanovska <natalibojko@gmail.com>
2016-04-08 23:12:12 +0300

    interface list by string

---
+++ Lesson13/Lesson12/WhiteFilter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson13
+{
+    public class WhiteFilter: IFilter
+    {
+      
+       public bool IsSatisfied(List<string> FilterList, string line)
+       {
+               return (FilterList.Contains(line));
+           
+       }
+    }
+}
